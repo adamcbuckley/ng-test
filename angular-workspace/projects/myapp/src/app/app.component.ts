76b83bd83653b040a809d2bc0a49ca4e2c9df1c3
Adam Buckley <git@adambuckley.net>
2024-02-27 18:39:05 +0000

    My app uses component from mylib

--- angular-workspace/projects/myapp/src/app/app.component.ts
+++ angular-workspace/projects/myapp/src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { MylibComponent } from "mylib";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, MylibComponent],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'myapp';
+  title = "myapp";
 }
