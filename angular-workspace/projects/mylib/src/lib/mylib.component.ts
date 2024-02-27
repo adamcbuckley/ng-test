ab749810be59122fa9923163473325ff000b68e6
Adam Buckley <git@adambuckley.net>
2024-02-27 18:20:36 +0000

    Added mylib

---
+++ angular-workspace/projects/mylib/src/lib/mylib.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "lib-mylib",
+  standalone: true,
+  imports: [],
+  template: `<h1>mylib works!</h1>`,
+  styles: ``,
+})
+export class MylibComponent {}
